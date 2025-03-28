18c88b8dd88698acd60e574a0bfb1550b73fab64
python-major <itumelengmajoro97@gmail.com>
2025-03-28 12:23:03 +0200

    modify next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript:
-        ignoreBuildErrors: true
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
 };
-eslint: {
-    ignoreDuringdBuilds: true
-}
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
